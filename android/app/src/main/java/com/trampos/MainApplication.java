881c8fb8a39d1090aebb1c388a5aa79404628526
LuisCarlosb3 <lu.ca.oliveira41@gmail.com>
2019-04-27 23:20:27 -0400

    configurating firebase

--- android/app/src/main/java/com/trampos/MainApplication.java
+++ android/app/src/main/java/com/trampos/MainApplication.java
@@ -3,6 +3,9 @@ package com.trampos;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,8 +28,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new VectorIconsPackage(),
-            new RNGestureHandlerPackage()
+            new RNGestureHandlerPackage(),
+             new RNFirebaseFirestorePackage() // firestore
       );
     }
 
